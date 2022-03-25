5439f3767c4561915a1d35eaf6600bdb0e49b0d1
jicu mollick <jikumollick82@gmail.com>
2022-03-25 14:08:53 +0600

    products added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './Card/Card';
 import Navbar from './Navbar/Navbar';
 function App() {
 
@@ -19,6 +20,13 @@ function App() {
     <div >
     
     <Navbar></Navbar>
+    {
+      guns.map( (gun) => {
+        return ( 
+        <Card key={gun.id} gunData={gun}></Card>
+        )
+      })
+    }
     </div>
   );
 }
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Card.css';
-const Card = () => {
+const Card = (props) => {
+    const {name,img, bullet, capacity , action , price} = props.gunData;
     return (
-        <div>
-            <h1>Card</h1>
+        <div className='card'>
+            <div className='image-container'>
+                <img src={img} alt="" />
+            </div>
+            <h1>{name}</h1>
+            <p>Bullet Type: {bullet}</p>
+            <p>Capacity: {capacity}</p>
+            <p>Action: {action}</p>
+            <div className="add-to-cart">
+                <button>Add to cart</button>
+                <h1>${price}</h1>
+            </div>
+            
         </div>
     );
 };
